0203adc11ceab23c2dac9758f21d34a889c7e707
Bilel Friaa <bilelfriaa@hotmail.fr>
2022-11-29 11:20:47 +0100

    début artist controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,37 @@
 class ArtistsController < ApplicationController
   def index
     @artists = Artist.all
+
+    if params[:category].present?
+      @artists = Artist.where(category: params[:category])
+    else
+      @artists = Artist.all
+    end
+  end
+
+  def show
+    @artit = Artist.find(params[:id])
+  end
+
+  def new
+    @artist = Artist.new
+    authorize @artist
+  end
+
+  def create
+    @artist = Artist.new(artist_params)
+    @artist.user = current_user
+    if @artist.save
+      redirect_to artists_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+    authorize @artist
+  end
+
+  private
+
+  def artist_params
+    params.require(:artist).permit(:first_name, :last_name, :date_of_birth, :category, :email)
   end
 end
